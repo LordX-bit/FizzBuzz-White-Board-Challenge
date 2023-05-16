5ba3adbe17362e758c1b649ee88415ea8aa188d3
Jose Zepeda <111955228+LordX-bit@users.noreply.github.com>
2023-05-16 15:17:08 -0700

    Add files via upload
    First Commit

---
+++ Fizzbuzz White Board Challenge.cs
@@ -0,0 +1,35 @@
+
+namespace SoloLearn
+
+    class program 
+    
+        static void Main(string[] args)
+        {
+            public string FizzBuzz(int number)
+            {
+                var answer = ""'
+               if(number % 3 == 0)
+               {
+                 answer == "fizz"
+               } 
+               if (number % 5 == 0)
+               {
+                   answer += "buzz"
+               }
+            }
+            retunr answer
+        }
+
+
+//Write a public method that accepts a number
+//access modifier
+//return type
+//Method Name
+// parameters 
+//Scope
+//when the number is divisible by 3 return the word fizz
+
+//when the number is divisible by 5 return the word buzz
+
+//when the number is divisible by both, return the word fizzbuzz
+    
